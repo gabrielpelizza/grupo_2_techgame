06db0b120dab8540a1802db685e5217c5c7cc074
GimenaRivoira <gimenarivoira@gmail.com>
2021-01-31 21:51:57 -0300

    archivos de registro agregado

--- tech-game/app.js
+++ tech-game/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var loginRouter = require('./routes/login')
+var registroRouter = require('./routes/registro')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/registro', registroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
