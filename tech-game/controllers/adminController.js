dc7dcb5331f7e45110596043a183596953d1fdde
GimenaRivoira <gimenarivoira@gmail.com>
2021-02-18 11:04:00 -0300

    crud delete eliminado

--- tech-game/controllers/adminController.js
+++ tech-game/controllers/adminController.js
@@ -1,5 +1,26 @@
+const path = require('path');
+
+let fs = require('fs');
+
+const {leerJSON} = require(path.join('..', 'data', 'products'));
+
+const productos = leerJSON();
+
 module.exports = {
     crud : (req, res, next)=>{
-        res.render('admin/crud');
-      }
+        res.render('admin/crud', {
+          productos
+        });
+      },
+    delete : (req, res, next) => {
+      productos.forEach(elemento => {
+        if(elemento.id == req.params.id){
+          let eliminar = productos.indexOf(elemento);
+          productos.splice(eliminar,1)
+        }
+      });
+
+      fs.writeFileSync('./data/products.json', JSON.stringify(productos), 'utf-8');
+      res.redirect('/admin')
+      } 
 }
