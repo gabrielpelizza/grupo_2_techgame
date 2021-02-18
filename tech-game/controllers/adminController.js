d1e620f402f85c48d5817bf63fa719feb8284ced
Your Namegabrielpelizza <you@example.comgbrpelizza@gmail.com>
2021-02-18 13:53:45 -0300

    se agrego creador de productos al crud

--- tech-game/controllers/adminController.js
+++ tech-game/controllers/adminController.js
@@ -2,17 +2,62 @@ const path = require('path');
 
 let fs = require('fs');
 
-const {leerJSON} = require(path.join('..', 'data', 'products'));
+const {getProduct, setProduct} = require(path.join('..', 'data', 'products'));
 
-const productos = leerJSON();
+
+
+const productos = getProduct();
 
 module.exports = {
     crud : (req, res, next)=>{
         res.render('admin/crud', {
           productos
         });
-      },
-    delete : (req, res, next) => {
+    },
+
+    createProduct : (req,res,next)=>{
+        
+        
+
+        let lastID = 1;
+        productos.forEach(producto => {
+            if (producto.id > lastID) {
+                lastID = producto.id
+            }else{
+              res.send("error ")
+            }
+        });
+        
+        const {nombre,precio,sku,stock,descuento,detalle,descripcion,img, categoria, marcas} = req.body;
+
+
+        const newProduct = {
+            id: Number(lastID + 1),
+            product_name : nombre,
+            description : descripcion,
+            sku : sku,
+            image : img,
+            stock : stock,
+            price: precio,
+            details : detalle,
+            category : categoria,
+            discount : descuento,
+            brand : marcas
+            /* img : req.files[0].filename */
+        }
+
+        productos.push(newProduct)
+
+        
+        setProduct(productos);
+
+        res.redirect('/admin',{
+          alerta
+        });
+
+    },
+
+    deleteProduct : (req, res, next) => {
       productos.forEach(elemento => {
         if(elemento.id == req.params.id){
           let eliminar = productos.indexOf(elemento);
@@ -22,5 +67,5 @@ module.exports = {
 
       fs.writeFileSync('./data/products.json', JSON.stringify(productos), 'utf-8');
       res.redirect('/admin')
-      } 
+    } 
 }
--- tech-game/data/products.js
+++ tech-game/data/products.js
@@ -3,7 +3,10 @@ const path = require('path');
 const productos_db = path.join('data', 'products.json');
 
 module.exports = {
-    leerJSON : () =>{
+    getProduct : () =>{
         return JSON.parse(fs.readFileSync(productos_db, 'utf-8'))
+    },
+    setProduct : (data) => {
+        return fs.writeFileSync(productos_db,JSON.stringify(data,null,2),'utf-8');
     }
 }
