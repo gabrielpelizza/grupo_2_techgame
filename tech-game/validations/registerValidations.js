442b2431b95faae8e7b9637f024350f0afdab446
GimenaRivoira <gimenarivoira@gmail.com>
2021-03-01 11:11:34 -0300

    se modifico registro, y ejs de perfil y detalle

--- tech-game/validations/registerValidations.js
+++ tech-game/validations/registerValidations.js
@@ -1,11 +1,28 @@
 const {check, validationResult, body} = require('express-validator');
 
 module.exports = [
-    check('name').notEmpty().withMessage('Debes completar el campo nombre'),
-    check('lastname').notEmpty().withMessage('Debes completar el campo apellido'),
-    check('email').notEmpty().isEmail().withMessage('Debes completar el campo email'),
-    check('dni').notEmpty().isNumeric().withMessage('Tiene que ser un dni valido'),
-    check('country').notEmpty().withMessage('Debes completar el campo localidad'),
-    check('password').notEmpty().withMessage('Debes ingresar una clave'),
-    check('password2').notEmpty().withMessage('Debes ingresar una clave')
+    check('name')
+    .notEmpty()
+    .withMessage('Debes completar el campo nombre'),
+
+    check('lastname')
+    .notEmpty()
+    .withMessage('Debes completar el campo apellido'),
+
+    check('email')
+    .notEmpty()
+    .isEmail()
+    .withMessage('Debes completar el campo email'),
+
+    check('dni')
+    .notEmpty()
+    .isNumeric()
+    .withMessage('Tiene que ser un dni valido'),
+
+    check('country')
+    .notEmpty()
+    .withMessage('Debes completar el campo localidad'),
+
+    body('password2').custom((value, {req})=> value !== req.body.password ? false : true)
+    .withMessage('Las contraseñas no coinciden')
   ]
