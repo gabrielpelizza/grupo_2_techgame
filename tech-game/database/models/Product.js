b3d0a87c97dab625909bac8d5f14245b7680dcc5
Your Namegabrielpelizza <you@example.comgbrpelizza@gmail.com>
2021-03-14 13:53:48 -0300

    se agrego modelo productos

---
+++ tech-game/database/models/Product.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, dataTypes) => {
+
+	let alias = 'products';
+	let cols = {
+		id: {
+		type: dataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+		},
+		product_name:{
+		type: dataTypes.STRING,
+        allowNull : false
+		} ,
+		product_name:{
+        type: dataTypes.STRING,
+        allowNull : false
+        } ,
+        sku : {
+            type: dataTypes.INTEGER,
+            allowNull : false 
+        } ,
+        stock : {
+            type: dataTypes.INTEGER,
+            allowNull : false
+        } ,
+        price : {
+            type: dataTypes.INTEGER,
+            allowNull : false
+        } ,
+        discount : {
+            type: dataTypes.INTEGER,
+            allowNull : false
+        } ,
+        brand_id : {
+            type: dataTypes.INTEGER,
+            allowNull : false
+        } ,
+        image : {
+            type: dataTypes.STRING,
+        }
+	};
+
+	let config = {
+		tableName : 'products', /* no es necesario ya que sequelize toma el nombre del archivo pero en plurar como el nombre de la tabla */
+		timestamps: false 
+	}
+
+	const Products = sequelize.define(alias,cols,config)
+	
+    Productos.associate = function(modelos){
+        Productos.hasMany(models.Brand,{
+            as : 'brand'
+        });
+    }
+
+	return Products;
+
+}
