ad562dffce302239fb9a2d76b67b41dc1c31b855
MolinaEmilce <molinaemilcedaiana@gmail.com>
2021-03-17 19:36:56 -0300

    productos editados correctamente

--- tech-game/routes/admin.js
+++ tech-game/routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const upload = require('../middlewares/subidaImagenes');
 const productValidator = require('../validations/productValidation');
 
-const {createProduct,productModificado,productAlmacenado, crud, deleteProduct, editarProducto,index, usuario} = require('../controllers/adminController');
+const {createProduct,productModificado,productAlmacenado, crud, deleteProduct,index, usuario, editProduct} = require('../controllers/adminController');
 
 router.get('/', index)
 router.get('/productos', crud);
@@ -12,9 +12,9 @@ router.get('/productos/create',createProduct) //muestra el formulario crear prod
 
 router.post('/productos/create', upload.any(), productValidator, productAlmacenado)//cumple la accion de almacenar el producto agregado
 
-/* router.delete('/productos/delete/:id', deleteProduct) */ //cumple la accion de eliminar producto
+router.delete('/productos/delete/:id', deleteProduct)  //cumple la accion de eliminar producto
 
-/* router.get('/productos/editar/:id', editarProducto) */ //muestra vista con datos de producto 
-/* router.put('/productos/update/:id',productModificado); */
+ router.get('/productos/editar/:id', editProduct)  //muestra vista con datos de producto 
+ router.put('/productos/update/:id',productModificado); 
 
 module.exports = router;
